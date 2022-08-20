4a5d75c263cf2bb1a59b5a5aebbe48e6f718fab0
Julian Quispe <julianmql@hotmail.com>
2022-08-20 12:00:04 -0500

    feat: exercise completed

--- src/com/julianqll/Main.java
+++ src/com/julianqll/Main.java
@@ -6,13 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite la medida del lado A:");
-        double a = sc.nextDouble();
-        System.out.println("Digite la medida del lado B:");
-        double b = sc.nextDouble();
-        System.out.println("Digite la medida del lado C:");
-        double c = sc.nextDouble();
-        double area = (((a-c)*b)/2) + c*b;
-        System.out.println("El area del terreno con las medidas ingresada es: " + area);
+        System.out.println("Digite el importe que aporta Carmela:");
+        double importeCarmela = sc.nextDouble();
+        System.out.println("Digite el importe que aporta Javier:");
+        double importeJavier = sc.nextDouble();
+        System.out.println("Digite el importe que aporta Eulogio:");
+        double importeEulogio = sc.nextDouble();
+        double capital = importeCarmela + importeJavier + importeEulogio;
+        double porcentajeCarmela = (importeCarmela/capital) * 100;
+        double porcentajeJavier = (importeJavier/capital) * 100;
+        double porcentajeEulogio = (importeEulogio/capital) * 100;
+        System.out.println("El capital formado es: " + capital);
+        System.out.println("El porcentaje del capital de Carmela es: " + porcentajeCarmela +"% \n" +
+                            "El porcentaje del capital de Javier es: " + porcentajeJavier +"% \n" +
+                            "El porcentaje del capital de Eulogio es: " + porcentajeEulogio +"% \n");
     }
 }
