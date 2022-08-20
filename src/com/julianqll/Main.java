22bd444d0cf08a2bf85a2e62b04536579d3bc6ef
Julian Quispe <julianmql@hotmail.com>
2022-08-20 11:50:00 -0500

    feat: exercise completed

--- src/com/julianqll/Main.java
+++ src/com/julianqll/Main.java
@@ -6,13 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite la medida del lado A:");
-        double a = sc.nextDouble();
-        System.out.println("Digite la medida del lado B:");
-        double b = sc.nextDouble();
-        System.out.println("Digite la medida del lado C:");
-        double c = sc.nextDouble();
-        double area = (((a-c)*b)/2) + c*b;
-        System.out.println("El area del terreno con las medidas ingresada es: " + area);
+        System.out.println("Digite la altura de la piscina: ");
+        double altura = sc.nextDouble();
+        System.out.println("Digite la anchura de la piscina: ");
+        double anchura = sc.nextDouble();
+        System.out.println("Digite el largo de la piscina: ");
+        double largo = sc.nextDouble();
+        System.out.println("Digite el precio por metro cúbico de agua: ");
+        double precio = sc.nextDouble();
+        double volumen = altura * anchura * largo;
+        double pago = volumen * precio;
+        System.out.println("El pago que se debe realizar por el total de m3 que consume de agua al llenar la piscina es: " + pago);
     }
 }
