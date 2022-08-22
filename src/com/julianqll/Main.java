a5726557c951882ee041a2fa3533a9e0ca51f064
Julian Quispe <julianmql@hotmail.com>
2022-08-22 18:28:39 -0500

    feat: exercise completed

--- src/com/julianqll/Main.java
+++ src/com/julianqll/Main.java
@@ -6,20 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite la ganancia de la empresa en el año:");
-        double gananciaAño = sc.nextDouble();
-        System.out.println("Digite la cantidad de acciones del primer socio:");
-        double accionesPrimerSocio = sc.nextDouble();
-        System.out.println("Digite la cantidad de acciones del segundo socio:");
-        double accionesSegundoSocio = sc.nextDouble();
-        System.out.println("Digite la cantidad de acciones del tercer socio");
-        double accionesTercerSocio = sc.nextDouble();
-        double totalAcciones = accionesPrimerSocio + accionesSegundoSocio + accionesTercerSocio;
-        double gananciaPrimerSocio = (accionesPrimerSocio/totalAcciones) * gananciaAño;
-        double gananciaSegundoSocio = (accionesSegundoSocio/totalAcciones) * gananciaAño;
-        double gananciaTercerSocio = (accionesTercerSocio/totalAcciones) * gananciaAño;
-        System.out.println("El monto de ganancia que le corresponde al primer socio es: " + gananciaPrimerSocio +" \n" +
-                            "El monto de ganancia que le corresponde al primer socio es: " + gananciaSegundoSocio +" \n" +
-                            "El monto de ganancia que le corresponde al primer socio es: " + gananciaTercerSocio +" \n");
+        System.out.println("Digite el precio de compra de la pieza:");
+        double precioCompra = sc.nextDouble();
+        System.out.println("Digite el porcentaje de ganancia que desea tener(%):");
+        double porcentajeGanancia = sc.nextDouble();
+        double precioVenta = precioCompra * (100+porcentajeGanancia)/100;
+        System.out.println("El precio al que debe vender la pieza para obtener la ganancia deseada es: " + precioVenta);
     }
 }
