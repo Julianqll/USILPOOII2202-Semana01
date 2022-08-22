03c9160188b587e2203d5859c64968f7eee4bd74
Julian Quispe <julianmql@hotmail.com>
2022-08-22 18:38:11 -0500

    feat: exercise completed

--- src/com/julianqll/Main.java
+++ src/com/julianqll/Main.java
@@ -6,11 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite el precio de compra de la pieza:");
-        double precioCompra = sc.nextDouble();
-        System.out.println("Digite el porcentaje de ganancia que desea tener(%):");
-        double porcentajeGanancia = sc.nextDouble();
-        double precioVenta = precioCompra * (100+porcentajeGanancia)/100;
-        System.out.println("El precio al que debe vender la pieza para obtener la ganancia deseada es: " + precioVenta);
+        System.out.println("Digite el precio del producto:");
+        double precioProducto = sc.nextDouble();
+        System.out.println("Digite la cantidad de unidades:");
+        double cantidadUnidades = sc.nextDouble();
+        System.out.println("Digite el porcetanje del descuento(%) sobre la compra:");
+        double porcentajeDescuento = sc.nextDouble();
+        double importeCompra = precioProducto * cantidadUnidades;
+        double importeDescuento = (porcentajeDescuento / 100) * importeCompra;
+        double importePagar = importeCompra - importeDescuento;
+        System.out.println("El importe de la compra es: " + importeCompra);
+        System.out.println("El importe del descuento es: " + importeDescuento);
+        System.out.println("El importe a pagar por la compra es: " + importePagar);
     }
 }
