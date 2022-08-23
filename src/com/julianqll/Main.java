ccaa3382bbbfc2869edef839892af2d06729bc3d
Julian Quispe <julianmql@hotmail.com>
2022-08-23 09:39:30 -0500

    feat: exercise completed

--- src/com/julianqll/Main.java
+++ src/com/julianqll/Main.java
@@ -6,11 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite la distancia entre ciudades:");
-        double distancia = sc.nextDouble();
-        System.out.println("Digite la velocidad que tiene la bicicleta:");
-        double velocidad = sc.nextDouble();
-        double tiempo = distancia / velocidad;
-        System.out.println("El tiempo que tarda la persona es: " + tiempo);
+        System.out.println("Digite la nota final del curso:");
+        double notaFinal = sc.nextDouble();
+        if (notaFinal < 12.50 && notaFinal >= 0)
+        {
+            System.out.println("Estado del alumno: DESAPROBADO");
+        }
+        else if (notaFinal >= 12.5 && notaFinal <= 20)
+        {
+            System.out.println("Estado del alumno: APROBADO");
+        }
+        else
+        {
+            System.out.println("Error: No existe estado del alumno");
+
+        }
     }
 }
