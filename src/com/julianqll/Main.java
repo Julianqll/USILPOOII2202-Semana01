2cfc97328edb29cf079bcfc9ddffb07abaed8376
Julian Quispe <julianmql@hotmail.com>
2022-08-23 09:32:31 -0500

    feat: exercise completed

--- src/com/julianqll/Main.java
+++ src/com/julianqll/Main.java
@@ -6,14 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite la hipotenusa(H) de la figura:");
-        double hipotenusa = sc.nextDouble();
-        System.out.println("Digite el radio(R) de la figura:");
-        double radio = sc.nextDouble();
-        double altura = Math.sqrt(Math.pow(hipotenusa,2) - Math.pow(radio,2)) ;
-        double areaTriangulo = (radio * altura) / 2;
-        double areaSemicircunferencia = (Math.PI * Math.pow(radio,2))/2;
-        double areaFormaA = (areaTriangulo * 2) + areaSemicircunferencia;
-        System.out.println("El area de la forma A es: " + areaFormaA);
+        System.out.println("Digite la distancia entre ciudades:");
+        double distancia = sc.nextDouble();
+        System.out.println("Digite la velocidad que tiene la bicicleta:");
+        double velocidad = sc.nextDouble();
+        double tiempo = distancia / velocidad;
+        System.out.println("El tiempo que tarda la persona es: " + tiempo);
     }
 }
